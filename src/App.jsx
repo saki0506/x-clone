bd5156752aa3b4199c73e53eb9b765c59a3df1b2
saki <172128609+saki0506@users.noreply.github.com>
2025-04-20 21:34:48 +0900

    "Add JSX example with conditional rendering"

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
 function App() {
+  const helloWorld = 'Hello, world!';
+  const loggedInText = 'You are logged in.';
+  const loggedOutText = 'You are not logged in.';
+  const isLoggedIn = false;
+  const isDone = false;
   return (
     <div className="flex flex-col items-center mt-32">
       <h1 className="text-3xl font-bold">
-        Hello, World!
+        {helloWorld}
       </h1>
+      {isLoggedIn ? (
+        <p className="text-green-500">{loggedInText}</p>
+      ) : (
+        <p className="text-red-500">{loggedOutText}</p>
+      )}
+
+      {isDone ?
+        <p className="text-blue-500">done</p> :
+        <p className="text-yellow-500">not yet</p>
+      }
     </div>
   );
 }
