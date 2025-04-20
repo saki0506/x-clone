1d0a9ee9a1a0c7e143ca7b4a244593260c069b07
saki <172128609+saki0506@users.noreply.github.com>
2025-04-20 23:21:10 +0900

    各ファイルへ追加

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,28 @@
 function App() {
-  const helloWorld = 'Hello, world!';
-  const loggedInText = 'You are logged in.';
-  const loggedOutText = 'You are not logged in.';
-  const isLoggedIn = false;
+  const helloWorld = "Hello, world!";
+  const loggedInText = "You are logged in.";
+  const loggedOutText = "You are not logged in.";
+  const isLoggedIn = true;
   const isDone = false;
+  const loginLabel = (isLoggedIn) => {
+    return isLoggedIn ? (
+      <p className="text-green-500">{loggedInText}</p>
+    ) : (
+      <p className="text-red-500">{loggedOutText}</p>
+    );
+  };
   return (
     <div className="flex flex-col items-center mt-32">
-      <h1 className="text-3xl font-bold">
-        {helloWorld}
-      </h1>
-      {isLoggedIn ? (
-        <p className="text-green-500">{loggedInText}</p>
-      ) : (
-        <p className="text-red-500">{loggedOutText}</p>
-      )}
+      <h1 className="text-3xl font-bold">{helloWorld}</h1>
+      {loginLabel(isLoggedIn)}
 
-      {isDone ?
-        <p className="text-blue-500">done</p> :
+      {isDone ? (
+        <p className="text-blue-500">done</p>
+      ) : (
         <p className="text-yellow-500">not yet</p>
-      }
+      )}
     </div>
   );
 }
 
-export default App
+export default App;
