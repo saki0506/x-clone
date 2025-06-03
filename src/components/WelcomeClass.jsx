9e87b4f55ab71420e49ff5a575f1c64b063331e7
saki0506 <zchitian364@gmail.com>
2025-06-03 23:51:30 +0900

    関数コンポーネントとクラスコンポーネント復習

--- src/components/WelcomeClass.jsx
+++ src/components/WelcomeClass.jsx
@@ -1,9 +1,9 @@
-import React from "react";      // ← クラス版は必須
+import React from "react"; // ← React.Component を使う場合は React のインポートが必須
 
-class WelcomeClass extends React.Component {
+class Welcome extends React.Component {
   render() {
     return <h1>Welcome to X Clone!</h1>;
   }
 }
 
-export default WelcomeClass;
+export default Welcome;
